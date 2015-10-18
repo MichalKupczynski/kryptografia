d4ae67227c129b060e76425a23b94f7694fdcbce
Michal Kupczynski <mkupczynski195@gmail.com>
2015-10-18 22:18:40 +0200

    cryptograms and config files

---
+++ lista2/zad1/zad1.py
@@ -0,0 +1,18 @@
+__author__ = 'michal'
+
+
+class ModuloTwo:
+
+    def xor(self, a, b):
+        if a == b:
+            return 0
+
+        else:
+            return 1
+
+class :
+
+    def __init__(self, entropy, ):
+
+
+    def
