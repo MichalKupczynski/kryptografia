cc70b28384b67a90f3b5593b32380735b009f412
Michal Kupczynski <mkupczynski195@gmail.com>
2015-10-18 23:08:27 +0200

    read cryptograms

--- lista2/zad1/zad1.py
+++ lista2/zad1/zad1.py
@@ -10,9 +10,53 @@ class ModuloTwo:
         else:
             return 1
 
-class :
+class Cryptograms:
 
-    def __init__(self, entropy, ):
+    def __init__(self, numberOfCyptograms, text = "text", cryptogram = "cryptogram" ):
 
+        self.NumberOfCryptograms = 0
+        self.text = []
+        with open(text) as file:
+            tmp = file.read()
+        tempString = str()
+        i = 0
+        for char in tmp:
+            if char == "0" or char == "1":
+                tempString = tempString + char
+                i +=1
+            if i == 8:
+                self.text.append(tempString)
+                tempString = str()
+                i = 0
 
-    def
+        self.cryptograms = []
+        for i in range(1, numberOfCyptograms):
+
+            self.cryptograms.append(self.readCryptogram(i, baseName = "kryptogram"))
+
+    def readCryptogram(self,n, baseName = "cryptogram"):
+
+        cryptogram = []
+        name = baseName + str(n)
+        print name
+        with open(name) as file:
+            tmp = file.read()
+        tempString = str()
+        i = 0
+        for char in tmp:
+            if char == "0" or char == "1":
+                tempString = tempString + char
+                i +=1
+            if i == 8:
+                cryptogram.append(tempString)
+                tempString = str()
+                i = 0
+        return cryptogram
+
+if __name__ == "__main__":
+
+    cryptograms = Cryptograms(20)
+    print cryptograms.text
+    for c in cryptograms.cryptograms:
+        print c
+
