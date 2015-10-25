09ffa01bb6e5f02bad213ab74e63b762a22d479c
Michal Kupczynski <mkupczynski195@gmail.com>
2015-10-25 20:36:53 +0100

    repair xor

--- lista2/zad1/zad1.py
+++ lista2/zad1/zad1.py
@@ -5,18 +5,19 @@ class ModuloTwo:
 
     def xor(self, a, b):
         if a == b:
-            return 0
+            return "0"
 
         else:
-            return 1
+            return "1"
 
     def xorBait(self, a, b):
 
         s = str()
-
+        print "a" +a
+        print "b" + b
+        print len(a)
         for i in range(0, len(a)):
             s = s + self.xor(a[i], b[i])
-
         return s
 
     def intToBait(self, i):
@@ -32,9 +33,9 @@ class ModuloTwo:
 
 class Cryptograms:
 
-    def __init__(self, numberOfCyptograms, text = "text", cryptogram = "cryptogram" ):
+    def __init__(self, numberOfCryptograms, text = "text", cryptogram = "cryptogram" ):
 
-        self.NumberOfCryptograms = 0
+        self.numberOfCryptograms = numberOfCryptograms
         self.text = []
         with open(text) as file:
             tmp = file.read()
@@ -50,7 +51,7 @@ class Cryptograms:
                 i = 0
 
         self.cryptograms = []
-        for i in range(1, numberOfCyptograms):
+        for i in range(1, self.numberOfCryptograms):
 
             self.cryptograms.append(self.readCryptogram(i, baseName = "kryptogram"))
 
@@ -76,7 +77,7 @@ class Cryptograms:
 
 class Decrypting:
 
-    def __init__(self, cryptograms, coding = "coding", entropy = "entropy"):
+    def __init__(self, cryptograms, coding = "coding", entropy_file = "entropy"):
 
         self.M2 = ModuloTwo()
         self.cryptograms = cryptograms
@@ -88,23 +89,28 @@ class Decrypting:
                 self.codingTable[line[0:i]] = line[i+1:]
                 self.decodingTable[line[i+1:]] = line[0:]
 
-        self.enropy = []
+        self.entropy = []
         self.q = 0
-        with open(entropy) as file:
+        with open(entropy_file) as file:
             for line in file.readline():
-                entropy.append[line]
-                q = q +1
+                self.entropy.append(line)
+                self.q = self.q +1
 
-    def decryptOneSign(self, n, q = self.q):
+    def decryptOneSign(self, n, q ):
 
         propositions = {}
         m = self.cryptograms.text[n]
         for i in range(0, self.cryptograms.numberOfCryptograms):
             try:
-                m1 = self.cryptograms.cryptograms[n][j]
+                m1 = self.cryptograms.cryptograms[i][n]
+                #print m1
+                #print "czesc"
                 result = self.M2.xorBait(m,m1)
+                print result
+                print "czesc"
                 for j in range(0, q):
                     s = self.entropy[j]
+                    print "hej"
                     if s in self.decodingTable:
                         c = self.M2.intToBait(s)
                         binaryR = self.M2.xorBait(result,c)
@@ -116,15 +122,17 @@ class Decrypting:
                                 propositions[R] = 1
             except:
                 pass
-
-        return sorted(propositions.iteritems(), key = lambda (k,v): v, reverse = True)[0][0]
+        print propositions
+        max = sorted(propositions.iteritems(), key = lambda (k,v): v, reverse = True)[0]
+        print max
+        return max
 
     def decryptMessage(self, result):
 
         Result = str()
         i = 0
         for s in self.cryptograms.text:
-            Result = Result + self.decryptOneSign(i)
+            Result = Result + self.decryptOneSign(i, self.q)
             i = i+1
         with open(result) as file:
             file.write(Result)
@@ -132,7 +140,6 @@ class Decrypting:
 if __name__ == "__main__":
 
     cryptograms = Cryptograms(20)
-    print cryptograms.text
-    for c in cryptograms.cryptograms:
-        print c
+    decrypting = Decrypting(cryptograms, "kodowanie", "entropia")
+    decrypting.decryptMessage("odczytany")
 
