a76a2e4e828859f3da52b4a38a469e628986be4a
Michal Kupczynski <mkupczynski195@gmail.com>
2015-11-14 22:39:43 +0100

    impoved argument parser

--- lista3/zad1.cpp
+++ lista3/zad1.cpp
@@ -13,47 +13,53 @@ int main(int argc, char* argv[])
 	
 	const char * Data = argv[1];
 	const char* Result = argv[2];
-	const char* option = argv[3];
-	const char* keystore_path = argv[4];
+	const char* keystore_path = argv[3];
+	const char* option = argv[4];
 	type = cbc128;
-	if(argc ==5)
+	std::cout<<argv[5];
+	if(argc >=5)
 	{
-		if (argv[4] == "cbc128")
+		std::cout << "tutaj";
+		if (argv[5] == "cbc128")
 			type = cbc128;
-		else if(argv[4] == "cbc192")
+		else if(argv[5] == "cbc192")
 			type = cbc192;
-		else if(argv[4] == "cbc256")
+		else if(argv[5] == "cbc256")
 			type = cbc256;
-		else if(argv[4] == "ecb128")
+		else if(argv[5] == "ecb128")
 			type = ecb128;
-		else if(argv[4] == "ecb192")
+		else if(argv[5] == "ecb192")
 			type = ecb192;
-		else if(argv[4] == "ecb256")
+		else if(argv[5] == "ecb256")
 			type = ecb256;
-		else if(argv[4] == "cfb128")
+		else if(argv[5] == "cfb128")
 			type = cfb128;
-		else if(argv[4] == "cfb192")
+		else if(argv[5] == "cfb192")
 			type = cfb192;
-		else if(argv[4] == "cfb256")
+		else if(argv[5] == "cfb256")
 			type = cfb256;
-		else if(argv[4] == "ofb128")
+		else if(argv[5] == "ofb128")
 			type = ofb128;
-		else if(argv[4] == "ofb192")
+		else if(argv[5] == "ofb192")
 			type = ofb192;
-		else if(argv[4] == "ofb256")
+		else if(argv[5] == "ofb256")
 			type = ofb256;
-			
+		else
+			std::cout<< "unknown parameter" << argv[5] << "type is set for cbc128" <<std::endl;
 	}
 	
+
+
 	in = fopen(Data, "r");
 	out = fopen(Result, "w");
 	int p = 1;
-	std::cout << option;
+
+	/**std::cout << option;
 	if (!std::string(option).compare("-d"))
 		p = 0;
 	std::cout <<p;
 	AesFileEnc Aes(type,keystore_path);
-    Aes.do_crypt(in, out, p);
+    Aes.do_crypt(in, out, p);*/
 
 	fclose(in);
 	fclose(out);
