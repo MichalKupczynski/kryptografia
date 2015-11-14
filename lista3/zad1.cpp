71243a965ee89cc015dfda56272cfc17b9b48112
Michal Kupczynski <mkupczynski195@gmail.com>
2015-11-14 23:01:33 +0100

    repair type parser

--- lista3/zad1.cpp
+++ lista3/zad1.cpp
@@ -19,33 +19,33 @@ int main(int argc, char* argv[])
 	std::cout<<argv[5];
 	if(argc >=5)
 	{
-		std::cout << "tutaj";
-		if (argv[5] == "cbc128")
+		std::string stype = std::string(argv[5]);
+		if (stype == "cbc128")
 			type = cbc128;
-		else if(argv[5] == "cbc192")
+		else if(stype == "cbc192")
 			type = cbc192;
-		else if(argv[5] == "cbc256")
+		else if(stype == "cbc256")
 			type = cbc256;
-		else if(argv[5] == "ecb128")
+		else if(stype == "ecb128")
 			type = ecb128;
-		else if(argv[5] == "ecb192")
+		else if(stype == "ecb192")
 			type = ecb192;
-		else if(argv[5] == "ecb256")
+		else if(stype == "ecb256")
 			type = ecb256;
-		else if(argv[5] == "cfb128")
+		else if(stype == "cfb128")
 			type = cfb128;
-		else if(argv[5] == "cfb192")
+		else if(stype == "cfb192")
 			type = cfb192;
-		else if(argv[5] == "cfb256")
+		else if(stype == "cfb256")
 			type = cfb256;
-		else if(argv[5] == "ofb128")
+		else if(stype == "ofb128")
 			type = ofb128;
-		else if(argv[5] == "ofb192")
+		else if(stype == "ofb192")
 			type = ofb192;
-		else if(argv[5] == "ofb256")
+		else if(stype == "ofb256")
 			type = ofb256;
 		else
-			std::cout<< "unknown parameter" << argv[5] << "type is set for cbc128" <<std::endl;
+			std::cout<< "unknown parameter " << argv[5] << " type is set for cbc128" <<std::endl;
 	}
 	
 
@@ -54,12 +54,12 @@ int main(int argc, char* argv[])
 	out = fopen(Result, "w");
 	int p = 1;
 
-	/**std::cout << option;
+	std::cout << option;
 	if (!std::string(option).compare("-d"))
 		p = 0;
 	std::cout <<p;
 	AesFileEnc Aes(type,keystore_path);
-    Aes.do_crypt(in, out, p);*/
+    Aes.do_crypt(in, out, p);
 
 	fclose(in);
 	fclose(out);
